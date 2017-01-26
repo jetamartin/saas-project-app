485affb21c75bdec7980010639597f25cf8c0740
jetamartin <jetamartin@yahoo.com>
2017-01-26 05:30:34 +0000

    Complete tenant and users

---
+++ app/controllers/confirmations_controller.rb
@@ -0,0 +1,81 @@
+class ConfirmationsController < Milia::ConfirmationsController
+    def update
+    if @confirmable.attempt_set_password(user_params)
+
+      # this section is patterned off of devise 3.2.5 confirmations_controller#show
+
+      self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+      yield resource if block_given?
+
+      if resource.errors.empty?
+        log_action( "invitee confirmed" )
+        set_flash_message(:notice, :confirmed) if is_flashing_format?
+          # sign in automatically
+        sign_in_tenanted_and_redirect(resource)
+        
+      else
+        log_action( "invitee confirmation failed" )
+        respond_with_navigational(resource.errors, :status => :unprocessable_entity){ render :new }
+      end
+
+    else
+      log_action( "invitee password set failed" )
+      prep_do_show()  # prep for the form
+      respond_with_navigational(resource.errors, :status => :unprocessable_entity){ render :show }
+    end  # if..then..else passwords are valid
+  end
+  
+  def show
+    if @confirmable.new_record?  ||
+       !::Milia.use_invite_member || 
+       @confirmable.skip_confirm_change_password
+
+      log_action( "devise pass-thru" )
+      # Next two lines added as mods to original Milia show method
+      self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+      yield resource if block_given?
+      
+      # Deleted the following line from original Milia show method
+      # super #this will redirect
+      
+      # Add "if" block below to original Milia code
+      if resource.errors.empty?
+        set_flash_message(:notice, :confirmed) if is_flashing_format?
+      end
+      
+      if @confirmable.skip_confirm_change_password
+        #original Melia code was follows:
+        # sign_in_tenanted(resource)
+        sign_in_tenanted_and_redirect(resource)
+      end
+    else
+      log_action( "password set form" )
+      # Added flash notice below to origina Milia code
+      flash[:notice] = "Please choose a password and confirm it"
+      prep_do_show()  # prep for the form
+    end
+    # else fall thru to show template which is form to set a password
+    # upon SUBMIT, processing will continue from update
+  end
+  
+  # Add below method to original Milia code
+  
+  def after_confirmation_path_for(resource_name, resource)
+    if user_signed_in?
+      root_path
+    else
+      new_user_session_path
+    end
+  end
+
+  private
+  
+  def set_confirmable()
+    # Made change to the original Milia @confirmable assignment statement that was as below:
+    # @confirmable = User.find_or_initialize_with_error_by(:confirmation_token, confirmation_token)
+    
+    # @confirmable assignment done made from params rather than confirmation_token as done in line above
+    @confirmable = User.find_or_initialize_with_error_by(:confirmation_token, params[:confirmation_token])
+  end
+end
+  
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,16 @@
 class HomeController < ApplicationController
+  # Doesn't require that the user be logged in to see the home page
   skip_before_action :authenticate_tenant!, :only => [ :index ]
 
   def index
+    if current_user
+      if session[:tenant_id]
+        Tenant.set_current_tenant session[:tenant_id]
+      else
+        Tenant.set_current_tenant current_user.tenants.first
+      end
+      @tentant = Tenant.current_tenant
+      params[:tenant_id] = @tenant.id
+    end
   end
 end
